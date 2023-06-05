088028e0a5a8bba6438ea14abcfca5aaf3265240
likhith <pemmareddylikhith@gmail.com>
2023-06-05 09:56:39 +0530

    deploy

--- recipe/settings.py
+++ recipe/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h71il!e_)3s4&^4og+g)e5)r1f3yqp#ppzo*1bl%7qdm***cy8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
